004d9798008df5045b1eb47fe141014688d6d862
zhangfan <zhangfan@zhangfandeMacBook-Pro.local>
2018-02-27 21:49:12 -0800

    a

--- data-producer.py
+++ data-producer.py
@@ -3,6 +3,8 @@
 # configurable stock symbole
 import json
 #
+import time
+import random
 import argparse
 import schedule 
 import time
@@ -30,6 +32,36 @@ def shutdown_hook(producer):
 	producer.close(10)
 	logger.info('kafka producer closed')
 
+
+def fetch_price_and_send(producer):
+	#fetch the stock data
+	#logger of data-producer here 
+	logger.debug('about to fetch price')
+	#trade_time = stock.get_trade_datetime()
+	trade_time = int(round(time.time() * 1000))#???
+	price = random.randint(30, 120)
+	data = {
+		#can use python help(stock) get what API has of this instance
+		'symbole': symbole,
+		'last_trade_time': trade_time,
+		'price': price
+	} 
+	#'' -> " "
+	# python --> string ""
+	data = json.dumps(data)
+	#send data to kafka
+	try:
+		#API: producer -> kafka : topic, data
+		producer.send(topic = topic_name, value = data)
+		logger.debug('finish sending data to kafka %s', data)
+	except Exception as e:
+		#it is not reliabe in the network!! so use try except .. ???  learn it 
+		logger.warn('fail to send price to kafka')
+
+
+	logger.info('retrieve stock price %s', data)
+
+"""
 def fetch_price_and_send(producer, stock):
 	#fetch the stock data
 	#logger of data-producer here 
@@ -57,7 +89,7 @@ def fetch_price_and_send(producer, stock):
 
 
 	logger.info('retrieve stock price %s', data)
-
+"""
 
 if __name__ == '__main__': # ???
 	parser = argparse.ArgumentParser()
@@ -70,7 +102,6 @@ if __name__ == '__main__': # ???
 	topic = args.topic
 	kafka_broker = args.kafka_broker
 
-	print args;
 
 	# kafka producer, read the document: kafka python
 
@@ -79,12 +110,12 @@ if __name__ == '__main__': # ???
 		bootstrap_servers = kafka_broker # what ??? is the mearning 
 	)
 
-	stock = Share(symbole) # yahoo finance, get the price according to the symbole
+	#stock = Share(symbole) # yahoo finance, get the price according to the symbole
 
 
 	# every 1 second, trigger fetch_price_and_send function, producer and stock are two parameters of it
 	# endless loop
-	schedule.every(1).second.do(fetch_price_and_send, producer, stock)
+	schedule.every(1).second.do(fetch_price_and_send, producer)
 	# close the producer before we run the the code
 	atexit.register(shutdown_hook, producer)##??? what is the order
 
